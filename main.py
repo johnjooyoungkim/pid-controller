78a372c30a7be52a6a68886323dbef932dbc5046
John Jooyoung Kim <118148365+johnjooyoungkim@users.noreply.github.com>
2026-08-10 11:38:21 +0000

    added simple sensor noise + track history of control terms

--- main.py
+++ main.py
@@ -7,21 +7,40 @@ from pid import PID
 dt = 0.1 # time step
 n = 1000  # number of steps
 plant = Plant(mass=1.0, friction=0.05, initial_state=[0.0, 1.0])
+
+# record of state change
 history = np.zeros((n,2))
 
-ctrl_P = PID(Kp=0.5, Ki=0.0, Kd=0.1)
-ctrl_I = PID(Kp=0.0, Ki=0.01, Kd=0.0)
-ctrl_D = PID(Kp=0.0, Ki=0.0, Kd=1.0)
-ctrl_PID = PID(Kp=0.5, Ki=0.0, Kd=0.2)
+# setpoint
+cmd = 100 
 
+# noise value (standard deviation)
+noise_std = 1
+
+# pid controller init
+ctrl_PID = PID(Kp=0.5, Ki=0.01, Kd=0.2)
+
+# step through 
 for i in range(n):
-    plant.step(ctrl_PID.compute(command=100, output=plant.state[0], dt=dt), dt) # compute control input based on position 100
+    noisy_state = plant.state[0] + np.random.normal(loc=0, scale=noise_std) # add sensor noise
+
+    plant.step(ctrl_PID.compute(command=cmd, output=noisy_state, dt=dt), dt) # compute control input based on position 100 
     history[i] = plant.state
 
 # plot dynamics
-fig, (ax1, ax2) = plt.subplots(2,1)
-ax1.plot(np.linspace(0,dt*n, n), history[:,0])
-ax2.plot(np.linspace(0,dt*n, n), history[:,1])
+fig, (ax_x, ax_v, ax_pid) = plt.subplots(3,1, figsize=(6,10))
+ax_x.plot(np.linspace(0,dt*n, n), history[:,0])
+ax_x.set_ylabel("position")
+ax_v.plot(np.linspace(0,dt*n, n), history[:,1])
+ax_v.set_ylabel("velocity")
 
+# plot PID values
+pid_history = ctrl_PID.return_history()
+ax_pid.plot(np.linspace(0,dt*n, n), pid_history[:,0],  label="P term")
+ax_pid.plot(np.linspace(0,dt*n, n), pid_history[:,1], label="I term")
+ax_pid.plot(np.linspace(0,dt*n, n), pid_history[:,2], label="D term")
+# ax.plot(np.linspace(0,dt*n, n), pid_history[:,3], label="total control", linestyle="--")
+ax_pid.legend()
+ax_pid.set_xlabel("timestep")
+ax_pid.set_ylabel("control contribution")
 plt.savefig('output.png')
-
--- pid.py
+++ pid.py
@@ -1,5 +1,7 @@
 # PID (Proportional-Integral-Derivative) Controller Class
 
+import numpy as np
+
 class PID: 
     def __init__(self, Kp, Ki, Kd):
         self.Kp = Kp  # Proportional gain
@@ -7,11 +9,29 @@ class PID:
         self.Kd = Kd  # Derivative gain
         self.integral = 0.0
         self.previous_error = 0.0
+        self.history = []
+
+        
 
     def compute(self, command, output, dt):
         error = command - output
         self.integral += error * dt
         derivative = (error - self.previous_error) / dt
         self.previous_error = error
-        return self.Kp * error + self.Ki * self.integral + self.Kd * derivative
+ 
+        P_term = self.Kp * error
+        I_term = self.Ki * self.integral
+        D_term = self.Kd * derivative
+        u = P_term + I_term + D_term
 
+        self.history.append([P_term, I_term, D_term, u])
+
+        return u
+
+    # params: none
+    # return: numpy array [P_term, I_term, D_term, u] 
+    def return_history(self):
+        return np.array(self.history)
+
+    def reset_history(self):
+        self.history = []

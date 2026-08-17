bug fixes + added derivative-on-measurement

main.py
@@ -8,10 +8,11 @@ dt = 0.1 # time step
 n = 3000  # number of steps
 
 # saturation
-saturation = 14.0
+u_max = 14
+u_min = -14
 
 plant = Plant_Car(mass=1.0, friction=0.05, initial_state=[0.0, 1.0])
-plant_drone = Plant_Drone(mass=1.0, friction=0.05, initial_state=[0.0, 0.0], saturation=saturation)
+plant_drone = Plant_Drone(mass=1.0, friction=0.05, initial_state=[0.0, 0.0], saturation=u_max)
 
 # record of state change
 history = np.zeros((n,2))
@@ -20,16 +21,17 @@ history = np.zeros((n,2))
 cmd = 10
 
 # noise value (standard deviation)
-noise_std = 0.1
+noise_std = 0
 
 # pid controller init
-ctrl_PID = PID(Kp=0.5, Ki=0.1, Kd=0.1)
+ctrl_PID = PID(Kp=0.51, Ki=0.02, Kd=0.5)
+
 
 # step through 
 for i in range(n):
     noisy_state = plant_drone.state[0] + np.random.normal(loc=0, scale=noise_std) # add sensor noise
 
-    plant_drone.step(ctrl_PID.compute(command=cmd, output=noisy_state, dt=dt, u_min=-saturation, u_max=saturation), dt) # compute control input based on position 100 
+    plant_drone.step(ctrl_PID.compute(command=cmd, output=noisy_state, dt=dt, u_min=u_min, u_max=u_max), dt) # compute control input based on position 100 
     history[i] = plant_drone.state
 
 # plot dynamics

pid.py
@@ -9,13 +9,21 @@ class PID:
         self.Kd = Kd  # Derivative gain
         self.integral = 0.0
         self.previous_error = 0.0
+        self.previous_output = None
         self.history = []
         self.error = []
         
 
     def compute(self, command, output, dt, u_min=None, u_max=None):
         error = command - output
-        derivative = (error - self.previous_error) / dt
+
+        # first call -> no spike on D-term
+        if self.previous_output is None:
+            self.previous_output = output
+
+        # Derivative
+        derivative = -(output - self.previous_output) / dt # derivative-on-output
+        self.previous_output = output
 
         temp_integral = self.integral + error * dt
 
@@ -28,6 +36,7 @@ class PID:
         if u_min is not None and u_max is not None:
 
             u_clamped = np.clip(u_unclamped, u_min, u_max)
+            
             if (u_clamped != u_unclamped) and u_unclamped * error > 0: # condition for clamp
                 u = u_clamped
             else:
@@ -41,8 +50,6 @@ class PID:
         self.previous_error = error
         self.history.append([P_term, I_term, D_term, u])
         self.error.append(error)
-
-
         return u
 
     # params: none